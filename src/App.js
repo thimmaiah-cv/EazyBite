5b014e164b29739be7cde5c4447f2dc8f800f6ef
Thej48 <chattangadathimmaiah@gmail.com>
2025-12-09 15:57:43 +0530

    Getting started on Project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-6xl'>Hello Thej</h1>
+      <h1 className='text-6xl'>Hello Thej..!</h1>
     </div>
   );
 }
