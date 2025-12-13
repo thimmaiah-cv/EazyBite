f8e9b81597f572a70529d1f442458247c88096fa
Thej48 <chattangadathimmaiah@gmail.com>
2025-12-13 21:01:54 +0530

    Working on DashboardContent - Working on it..!

--- src/pages/admin/DashboardContent.jsx
+++ src/pages/admin/DashboardContent.jsx
@@ -63,7 +63,86 @@ export default function DashboardContent() {
           <span className="block font-raleway font-medium text-xl text-[#1F2932]">
             Popular Items
           </span>
+          <div className="flex flex-col gap-y-3 mt-3">
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-3">
+                <span className="bg-[#1F2932] flex justify-center items-center text-white font-roboto font-semibold text-xl h-10 w-10 text-center rounded-full">
+                  1
+                </span>
+                <div className="flex flex-col">
+                  <span className="text-[#1F2932] font-raleway font-semibold text-[17px]">
+                    Caesar Salad
+                  </span>
+                  <span className="text-[#717182] font-roboto font-normal text-[15px]">
+                    ₹ 8.99
+                  </span>
+                </div>
+              </div>
+              <span className="text-[#272744] text-lg font-raleway font-medium bg-[#ECEEF2] rounded-[10px] px-2.5 py-1">
+                <span className="font-roboto font-normal">2</span> orders
+              </span>
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-3">
+                <span className="bg-[#1F2932] flex justify-center items-center text-white font-roboto font-semibold text-xl h-10 w-10 text-center rounded-full">
+                  2
+                </span>
+                <div className="flex flex-col">
+                  <span className="text-[#1F2932] font-raleway font-semibold text-[17px]">
+                    Coca Cola
+                  </span>
+                  <span className="text-[#717182] font-roboto font-normal text-[15px]">
+                    ₹ 2.50
+                  </span>
+                </div>
+              </div>
+              <span className="text-[#272744] text-lg font-raleway font-medium bg-[#ECEEF2] rounded-[10px] px-2.5 py-1">
+                <span className="font-roboto font-normal">2</span> orders
+              </span>
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-3">
+                <span className="bg-[#1F2932] flex justify-center items-center text-white font-roboto font-semibold text-xl h-10 w-10 text-center rounded-full">
+                  3
+                </span>
+                <div className="flex flex-col">
+                  <span className="text-[#1F2932] font-raleway font-semibold text-[17px]">
+                    Margherita Pizza
+                  </span>
+                  <span className="text-[#717182] font-roboto font-normal text-[15px]">
+                    ₹ 12.99
+                  </span>
+                </div>
+              </div>
+              <span className="text-[#272744] text-lg font-raleway font-medium bg-[#ECEEF2] rounded-[10px] px-2.5 py-1">
+                <span className="font-roboto font-normal">1</span> orders
+              </span>
+            </div>
+
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-3">
+                <span className="bg-[#1F2932] flex justify-center items-center text-white font-roboto font-semibold text-xl h-10 w-10 text-center rounded-full">
+                  4
+                </span>
+                <div className="flex flex-col">
+                  <span className="text-[#1F2932] font-raleway font-semibold text-[17px]">
+                    Chicken Burger
+                  </span>
+                  <span className="text-[#717182] font-roboto font-normal text-[15px]">
+                    ₹ 15.99
+                  </span>
+                </div>
+              </div>
+              <span className="text-[#272744] text-lg font-raleway font-medium bg-[#ECEEF2] rounded-[10px] px-2.5 py-1">
+                <span className="font-roboto font-normal">1</span> orders
+              </span>
+            </div>
+          </div>
         </div>
+
+        {/*  */}
         <div className="flex flex-col bg-white border-2 border-[#E5E5E5] rounded-[20px] h-full w-full px-4 py-5">
           <span className="block font-raleway font-medium text-xl text-[#1F2932]">
             Recent Orders
